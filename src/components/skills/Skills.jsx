8805b9cb3c543b919cbcf5128352f21b58532e83
Ikram Hafidz <69641252+krumbs8749@users.noreply.github.com>
2023-01-30 01:13:11 +0100

    Styling for skills in Mobile View

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./skills.scss"
 import {Icon_javascript, Icon_cplusplus, Icon_java, Icon_php, Icon_html, Icon_scss, Icon_solidity} from './skills_assets/icon_languages'
 import {Icon_react, Icon_mysql, Icon_d3} from './skills_assets/Icon_technologies'
 
 function Skills() {
+    const [matches, setMatches] = useState(
+        window.matchMedia("(min-width: 768px)").matches
+    )
+
+    useEffect(() => {
+        window
+            .matchMedia("(min-width: 768px)")
+            .addEventListener('change', e => setMatches( e.matches ));
+    }, []);
   return (
     <div className='Skills'>
         <h1>Skills</h1>
         <div className='container'>
             <div className="groups">
-                <span>Languages</span>
-                <span>Technologies</span>
+                <span>{matches ? 'Languages' : 'Lang'}</span>
+                <span>{matches ? 'Technologies' : 'Tech'}</span>
                 {/* <span>Experience</span> */}
             </div>
             <div className="items">
